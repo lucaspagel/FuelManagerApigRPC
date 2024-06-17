c1aa6da800ba547e69b1eb00cd968b10e29a1430
lucaspagel <lucas.pagel0@gmail.com>
2024-06-17 14:51:06 -0300

    C# Implementation

---
+++ FuelManagerGraphQL/Models/Mutation.cs
@@ -0,0 +1,55 @@
+﻿namespace FuelManagerGraphQL.Models
+{
+    public class Mutation
+    {
+        public async Task<Veiculo> AddVeiculo([Service] AppDbContext _context, Veiculo model)
+        {
+            _context.Veiculos.Add(model);
+            await _context.SaveChangesAsync();
+
+            return model;
+        }
+
+        public async Task<Veiculo> UpdateVeiculo([Service] AppDbContext _context, Veiculo model)
+        {
+            _context.Veiculos.Update(model);
+            await _context.SaveChangesAsync();
+
+            return model;
+        }
+
+        public async Task<Veiculo> DeleteVeiculo([Service] AppDbContext _context, int id)
+        {
+            var model = await _context.Veiculos.FindAsync(id);
+            _context.Veiculos.Remove(model);
+            await _context.SaveChangesAsync();
+
+            return model;
+        }
+
+        public async Task<Consumo> AddConsumo([Service] AppDbContext _context, Consumo model)
+        {
+            _context.Consumos.Add(model);
+            await _context.SaveChangesAsync();
+
+            return model;
+        }
+
+        public async Task<Consumo> UpdateConsumo([Service] AppDbContext _context, Consumo model)
+        {
+            _context.Consumos.Update(model);
+            await _context.SaveChangesAsync();
+
+            return model;
+        }
+
+        public async Task<Consumo> DeleteConsumo([Service] AppDbContext _context, int id)
+        {
+            var model = await _context.Consumos.FindAsync(id);
+            _context.Consumos.Remove(model);
+            await _context.SaveChangesAsync();
+
+            return model;
+        }
+    }
+}
---
+++ FuelManagerGraphQL/Models/Query.cs
@@ -0,0 +1,21 @@
+﻿namespace FuelManagerGraphQL.Models
+{
+    public class Query
+    {
+        [UseProjection]
+        [UseFiltering]
+        [UseSorting]
+        public IQueryable<Veiculo> Veiculos([Service] AppDbContext _context)
+        {
+            return _context.Veiculos;
+        }
+
+        [UseProjection]
+        [UseFiltering]
+        [UseSorting]
+        public IQueryable<Consumo> Consumos([Service] AppDbContext _context)
+        {
+            return _context.Consumos;
+        }
+    }
+}
